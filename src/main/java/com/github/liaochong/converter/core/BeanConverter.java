bc81fb1b06580d4836f3ed06383c7610d484090f
liaochong <liaochong@guahao.com>
2017-11-19 17:26:04 +0800

    增加多个方法

--- src/main/java/com/github/liaochong/converter/core/BeanConverter.java
+++ src/main/java/com/github/liaochong/converter/core/BeanConverter.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -37,10 +38,43 @@ public class BeanConverter {
      * @return 结果
      */
     public static <E, T> List<E> convert(List<T> source, Class<E> targetClass) {
+        return convertList(source, targetClass, null);
+    }
+
+    /**
+     * 集合转换
+     * 
+     * @param source 需要转换的集合
+     * @param targetClass 需要转换到的类型
+     * @param supplier 异常操作
+     * @param <E> 转换后的类型
+     * @param <T> 转换前的类型
+     * @param <G> 异常返回类型
+     * @return 结果
+     */
+    public static <E, T, G extends RuntimeException> List<E> convertIfNullThrow(List<T> source, Class<E> targetClass,
+            Supplier<G> supplier) {
+        return convertList(source, targetClass, supplier);
+    }
+
+    /**
+     * 集合转换
+     *
+     * @param source 需要转换的集合
+     * @param targetClass 需要转换到的类型
+     * @param supplier 异常操作
+     * @param <E> 转换后的类型
+     * @param <T> 转换前的类型
+     * @param <G> 异常返回类型
+     * @return 结果
+     */
+    private static <E, T, G extends RuntimeException> List<E> convertList(List<T> source, Class<E> targetClass,
+            Supplier<G> supplier) {
         if (CollectionUtils.isEmpty(source)) {
             return Collections.emptyList();
         }
-        return source.stream().map(convertedObj -> convert(convertedObj, targetClass)).collect(Collectors.toList());
+        return source.stream().map(convertedObj -> convertBean(convertedObj, targetClass, supplier))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -70,10 +104,42 @@ public class BeanConverter {
      * @return 结果
      */
     public static <E, T> List<E> parallelConvert(List<T> source, Class<E> targetClass) {
+        return parallelConvertList(source, targetClass, null);
+    }
+
+    /**
+     * 集合并行转换，出现null值时抛出异常
+     * 
+     * @param source 需要转换的集合
+     * @param targetClass 需要转换到的类型
+     * @param supplier 异常操作
+     * @param <E> 转换后的类型
+     * @param <T> 转换前的类型
+     * @param <G> 异常返回类型
+     * @return 结果
+     */
+    public static <E, T, G extends RuntimeException> List<E> parallelConvertIfNullThrow(List<T> source,
+            Class<E> targetClass, Supplier<G> supplier) {
+        return parallelConvertList(source, targetClass, supplier);
+    }
+
+    /**
+     * 列表并行转换
+     * 
+     * @param source 需要转换的集合
+     * @param targetClass 需要转换到的类型
+     * @param supplier 异常操作
+     * @param <E> 转换后的类型
+     * @param <T> 转换前的类型
+     * @param <G> 异常返回类型
+     * @return 结果
+     */
+    public static <E, T, G extends RuntimeException> List<E> parallelConvertList(List<T> source, Class<E> targetClass,
+            Supplier<G> supplier) {
         if (CollectionUtils.isEmpty(source)) {
             return Collections.emptyList();
         }
-        return source.parallelStream().map(convertedObj -> convert(convertedObj, targetClass))
+        return source.parallelStream().map(convertedObj -> convertBean(convertedObj, targetClass, supplier))
                 .collect(Collectors.toList());
     }
 
@@ -95,7 +161,7 @@ public class BeanConverter {
     }
 
     /**
-     * 转换
+     * 单个Bean转换
      *
      * @param source 被转换对象
      * @param targetClass 需要转换到的类型
@@ -104,8 +170,44 @@ public class BeanConverter {
      * @return 结果
      */
     public static <E, T> E convert(T source, Class<E> targetClass) {
+        return convertBean(source, targetClass, null);
+    }
+
+    /**
+     * 单个Bean转换
+     *
+     * @param source 被转换对象
+     * @param targetClass 需要转换到的类型
+     * @param supplier 异常操作
+     * @param <E> 转换后的类型
+     * @param <T> 转换前的类型
+     * @param <G> 异常返回类型
+     * @return 结果
+     */
+    public static <E, T, G extends RuntimeException> E convertIfNullThrow(T source, Class<E> targetClass,
+            Supplier<G> supplier) {
+        return convertBean(source, targetClass, supplier);
+    }
+
+    /**
+     * 单个Bean转换
+     * 
+     * @param source 被转换对象
+     * @param targetClass 需要转换到的类型
+     * @param supplier 异常操作
+     * @param <E> 转换后的类型
+     * @param <T> 转换前的类型
+     * @param <G> 异常返回类型
+     * @return 结果
+     */
+    private static <E, T, G extends RuntimeException> E convertBean(T source, Class<E> targetClass,
+            Supplier<G> supplier) {
         if (Objects.isNull(source)) {
-            return null;
+            if (Objects.isNull(supplier)) {
+                return null;
+            } else {
+                throw supplier.get();
+            }
         }
         ObjectValidator.ifNullThrow(targetClass, () -> InvalidParameterException.of("targetClass can not be null"));
 
